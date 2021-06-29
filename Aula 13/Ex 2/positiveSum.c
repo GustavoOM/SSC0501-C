2be5694b3630770049137632bb45e15de9659bef
GustavoOM <gumartins2001@gmail.com>
2021-06-28 21:57:45 -0300

    Correct ex2

--- Aula 13/Ex 2/positiveSum.c
+++ Aula 13/Ex 2/positiveSum.c
@@ -2,23 +2,22 @@
 #include <stdlib.h>
 
 int main(){
-    void aloca(int *v, int n);
+    void aloca(int **v, int n);
     int sumPositiveNumbers(int *v, int n);
 
     int n, sum, *v;
     scanf("%d", &n);
-    aloca(v,n);
+    aloca(&v,n);
     sum = sumPositiveNumbers(v,n);
     printf("%d\n", sum);
     free(v);
     return 0;
 }
 
-void aloca(int *v, int n){
-    v = (int*) calloc(n, sizeof(int));
-    for(int i = 0; i < n; i++){
-        scanf("%d", &v[i]);
-    }
+void aloca(int **v, int n){
+    *v = (int*) calloc(n, sizeof(int));
+    for(int i = 0; i < n; i++)
+        scanf("%d", (*v + i));
 }
 
 int sumPositiveNumbers(int *v, int n){
